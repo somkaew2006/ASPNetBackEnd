66bc43af2baba16293380ca11798f23f9e71fa56
OFM\Teerayut.So <Teerayut.So@ofm.co.th>
2022-02-26 20:03:02 +0700

    new project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using ASPNETLIVE.Services.ThaiDate;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETLIVE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IThaiDate _thaidate;
+        public HomeController(IThaiDate thaiDate)
+        {
+            _thaidate = thaiDate;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var myThaiDate = _thaidate.ShowThaiDate();
+            return Ok(new {message= $"Hello MY API {myThaiDate}"});
+            
+        }
+    }
+}
