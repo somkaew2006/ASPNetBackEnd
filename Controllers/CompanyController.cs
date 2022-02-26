2f449e515b1c7bb1245a845ce644c8b49fde8662
OFM\Teerayut.So <Teerayut.So@ofm.co.th>
2022-02-27 00:14:57 +0700

    create model

---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETLIVE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        //https://localhost:7000/api/company
+        [Route("")]
+        [HttpGet]
+        public  IActionResult Hello()
+        {
+            return Ok(new { message = "Hello .NET" });
+        }
+
+        //https://localhost:7000/api/company/show/10
+        [Route("show/{id}")]
+        [HttpGet]
+        public IActionResult Show(int id)
+        {
+            return Ok(new { message = $"Hello {id}" });
+        }
+
+        //https://localhost:7000/api/company/search?name=cct&age=10
+        [Route("search")]
+        [HttpGet]
+        public IActionResult Search(string name,int? age=10)
+        {
+            return Ok(new { message = $"Hello {name} age : {age}" });
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASPNETLIVE.Models
+{
+    //[Table("categories")]
+    public class Category
+    {
+        //[Column("cat_id")]
+        //[Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryId { get; set; }
+
+        //[Column(TypeName ="ntext")]
+        [Required(ErrorMessage ="ชื่อประเภทสินค้า ห้ามว่าง")]
+        public string CategoryName { get; set; } = null!;
+
+        public bool IsActive { get; set; } = true;
+
+    }
+}
